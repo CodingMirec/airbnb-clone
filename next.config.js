35cd089132036d68fb4730fdf284488aeb26eb29
CodingMirec <m.sykora01@gmail.com>
2021-10-26 21:38:38 +0200

    setting up map

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoidGhlbWlyZWMiLCJhIjoiY2t2OGVxeGlpMGx0ZjJ2bWc3bzJnZjhmYyJ9.E76-xHtFzQ4iSd9QWvgAHA",
+  },
 };
