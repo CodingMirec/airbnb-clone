6f96628de6c536223cd1b6b31d4fc3319d44c44c
CodingMirec <m.sykora01@gmail.com>
2021-11-14 23:29:14 +0100

    navbar transparent

--- components/Header.js
+++ components/Header.js
@@ -11,6 +11,7 @@ import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { DateRangePicker } from "react-date-range";
 import { useRouter } from "next/dist/client/router";
+import Banner from "./Banner";
 
 function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState("");
@@ -21,6 +22,7 @@ function Header({ placeholder }) {
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
+    i;
     setEndDate(ranges.selection.endDate);
   };
 
@@ -47,7 +49,7 @@ function Header({ placeholder }) {
   };
 
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+    <header className="fixed top-0 z-50 grid grid-cols-3 bg-white bg-opacity-0 shadow-md p-5 md:px-10">
       <div
         onClick={() => router.push("/")}
         className="relative flex items-center h-10 cursor-pointer my-auto"
